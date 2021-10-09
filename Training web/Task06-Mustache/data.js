514ea3f944b8cb60481ec0255ef9ffaf5794bc01
Kevin <kevinarias66@gmail.com>
2021-10-09 15:23:26 -0600

    added handlebars to the base project

---
+++ Training web/Task06-Mustache/data.js
@@ -0,0 +1,44 @@
+export const defaultData = [
+  {
+    title: "Chuck Norris Bodyguards",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quo amet non distinctio temporibus in placeat, ullam omnis reprehenderit. Aliquid necessitatibus dolorem tenetur, ratione.",
+    image: "./images/chuck-norris-logo.png",
+    price: "$15 / 12 horas"
+  },
+  {
+    title: "Chuck Norris Bodyguards",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quo amet non distinctio temporibus in placeat, ullam omnis reprehenderit. Aliquid necessitatibus dolorem tenetur, ratione.",
+    image: "./images/chuck-norris-logo.png",
+    price: "$15 / 12 horas"
+  },
+  {
+    title: "Chuck Norris Bodyguards",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quo amet non distinctio temporibus in placeat, ullam omnis reprehenderit. Aliquid necessitatibus dolorem tenetur, ratione.",
+    image: "./images/chuck-norris-logo.png",
+    price: "$15 / 12 horas"
+  },
+  {
+    title: "Chuck Norris Bodyguards",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quo amet non distinctio temporibus in placeat, ullam omnis reprehenderit. Aliquid necessitatibus dolorem tenetur, ratione.",
+    image: "./images/chuck-norris-logo.png",
+    price: "$15 / 12 horas"
+  },
+  {
+    title: "Chuck Norris Bodyguards",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quo amet non distinctio temporibus in placeat, ullam omnis reprehenderit. Aliquid necessitatibus dolorem tenetur, ratione.",
+    image: "./images/chuck-norris-logo.png",
+    price: "$15 / 12 horas"
+  },
+  {
+    title: "Chuck Norris Bodyguards",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quo amet non distinctio temporibus in placeat, ullam omnis reprehenderit. Aliquid necessitatibus dolorem tenetur, ratione.",
+    image: "./images/chuck-norris-logo.png",
+    price: "$15 / 12 horas"
+  }
+];
